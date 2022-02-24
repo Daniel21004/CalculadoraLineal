cc9316623780d65f5395b5387f00bd7ccc39159f
Daniel21 <jorgedanielortega2004@gmail.com>
2022-02-24 15:53:23 -0500

    Programacion de los test para comprobar la correcta resolucion de la matriz inversa

--- src/main/java/com/mycompany/calculadoralineal/controllers/MatrizController.java
+++ src/main/java/com/mycompany/calculadoralineal/controllers/MatrizController.java
@@ -5,29 +5,33 @@ import com.mycompany.calculadoralineal.views.ResultsMatriz;
 
 public class MatrizController {
 
-    private int dimension;
-    MatrizInversa newMatrizInversa;
-    ResultsMatriz matrices = new ResultsMatriz();
+	private int dimension;
+	MatrizInversa newMatrizInversa;
+	ResultsMatriz matrices = new ResultsMatriz();
 
-    public MatrizController() {
-        this.dimension = matrices.pedirDimension();
-        verificarDimensiones();
-        matrices.presentarMatriz(newMatrizInversa.getMatrizA(), "A");
-        newMatrizInversa.calcularMatrizInversa();
-        matrices.presentarMatriz(newMatrizInversa.getMatrizUnidad(), "Inversa");
-    }
-    
-    private void verificarDimensiones() {
-        if (dimension == 2 || dimension == 3) {
-            inicializarMatriz();
-        } else {
-            System.out.println("Las dimensiones que se ha ingresado son invalidas.\nSolo se aceptan matrices de dimension 2 y 3, por favor vuelva a intentarlo");
-            System.out.println("");
-            dimension = 0;
-        }
-    }
-    private void inicializarMatriz() {
-        newMatrizInversa = new MatrizInversa(dimension);
-        newMatrizInversa.rellenarMatriz();
-    }
+	public MatrizController() {
+		this.dimension = matrices.pedirDimension();
+		inicializarMatriz();
+		matrices.presentarMatriz(newMatrizInversa.getMatrizA(), "A");
+		newMatrizInversa.calcularMatrizInversa();
+		matrices.presentarMatriz(newMatrizInversa.getMatrizUnidad(), "Inversa");
+	}
+
+	private boolean verificarDimensiones() {
+		if (dimension == 2 || dimension == 3) {
+			return true;
+		} else {
+			System.out.println("Las dimensiones que se ha ingresado son invalidas.\nSolo se aceptan matrices de dimension 2 y 3, por favor vuelva a intentarlo");
+			System.out.println("");
+			dimension = 0;
+			return false;
+		}
+	}
+
+	private void inicializarMatriz() {
+		if (verificarDimensiones()) {
+			newMatrizInversa = new MatrizInversa(dimension);
+			newMatrizInversa.rellenarMatriz();
+		}
+	}
 }
--- src/test/java/MatrizTest.java
+++ src/test/java/MatrizTest.java
@@ -1,13 +1,17 @@
 import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.mycompany.calculadoralineal.controllers.MatrizController;
 import com.mycompany.calculadoralineal.modelos.MatrizInversa;
 
 public class MatrizTest {
 
 	MatrizInversa matrizInversa;
+	MatrizController matrizController;
+	private int dimension;
 
 	@Before
 	public void before() {
@@ -16,10 +20,11 @@ public class MatrizTest {
 
 	@Test
 	public void testGenerarMatrizA() {
-		double[][] matrizPrueba_A = new double[][] { { 1, 2 }, { 3, 4 } };
 		matrizInversa.rellenarMatriz();
+		double[][] matrizPrueba_A = new double[][] { { 1, 2 }, { 3, 4 } };
 		assertArrayEquals(matrizPrueba_A[0], matrizInversa.getMatrizA()[0], 0);
 		assertArrayEquals(matrizPrueba_A[1], matrizInversa.getMatrizA()[1], 0);
+		testVerificarMatrizInversa();
 	}
 
 	@Test
@@ -28,5 +33,13 @@ public class MatrizTest {
 		assertArrayEquals(matrizPruebaIdentidad[0], matrizInversa.getMatrizUnidad()[0], 0);
 		assertArrayEquals(matrizPruebaIdentidad[1], matrizInversa.getMatrizUnidad()[1], 0);
 	}
-
+	
+	@Test
+	public void testVerificarMatrizInversa() {
+		matrizInversa.rellenarMatriz();
+		double[][] matrizInversaPrueba = new double[][]{{-2,1},{1.5,-0.5}};
+		matrizInversa.calcularMatrizInversa();
+		assertArrayEquals(matrizInversaPrueba[0], matrizInversa.getMatrizUnidad()[0],0);
+		assertArrayEquals(matrizInversaPrueba[1], matrizInversa.getMatrizUnidad()[1],0);
+	}
 }
